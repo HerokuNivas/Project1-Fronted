cdf3f1587199028d04f21432aad58872d2934768
Nivas <heroku.nivas.deployments@gmail.com>
2022-10-25 11:05:49 +0530

    Added all necessary navbar values

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homeroute from './Routes/Homeroute';
 import Documentation from './components/Documentation';
+import HomeRoute from "./components/HomeRoute";
+
 function App() {
   return (
 <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homeroute />}></Route>
+        <Route path="/home" element={<HomeRoute/>}></Route>
         <Route path="/documentation" element={<Documentation/>}></Route>
       </Routes>
     </BrowserRouter>
--- src/components/Documentation.jsx
+++ src/components/Documentation.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Navbar2 from "./Navbar2";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import '../css/Documentation.css';
+import { Link } from "react-router-dom";
 
 function Documentation(){
     return(
-        <Navbar2/>    
+        <div>
+            <Navbar2/>
+            <div className="arrowBackIconDoc">
+               <Link to="/home"><ArrowBackIcon style={{color: "#16cdfa", marginTop: "10px", marginLeft: "18px"}}/></Link>
+            </div>
+        </div>
+
     )
 }
 
