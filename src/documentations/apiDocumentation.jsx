9bd06800e0062dc12cf34e4e182df17cd47c2b8a
Nivas <heroku.nivas.deployments@gmail.com>
2022-10-25 12:30:10 +0530

    Added api completely

--- src/documentations/apiDocumentation.jsx
+++ src/documentations/apiDocumentation.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import '../css/apiDocumentation.css';
 import image1 from '../images/apiImage1.png';
-
+import image2 from '../images/apiImage2.png';
+import image3 from '../images/apiImage3.png';
 function ApiDocumentation(){
     return(
         <div className="apidoc">
@@ -12,7 +13,9 @@ function ApiDocumentation(){
             <p>3. Click on Geneate New Token and then Generate new token (classic). </p>
             <p>4. Expiration can be set as per your wish but make sure that the API key is active while you are using it to generate Inverted Index File.  </p>
             <p style={{marginLeft: "25px"}}><span style={{color: "red", fontWeight:"bold"}}>Note</span> : While generating token make sure that the first scope repo is checked. Without this the generated index file cannot be written back to your repository.</p>
-            <img src={image1} width={"50%"} height={"50%"}/>            
+            <div><img src={image1} width={"60%"} height={"60%"}/></div>
+            <div style={{marginTop: "1.8rem"}} ><img src={image2} width={"30%"} height={"30%"}/></div>
+            <div style={{marginTop: "1.8rem"}} ><img src={image3} width={"60%"} height={"60%"}/></div>
         </div>
     )
 }
