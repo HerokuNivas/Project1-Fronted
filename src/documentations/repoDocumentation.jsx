f59c5c0cedbd95d8cc2d78600f4d3d7fed3f9132
HerokuNivas <heroku.nivas.deployments@gmail.com>
2022-10-25 14:42:38 +0530

    Added the files

--- src/documentations/repoDocumentation.jsx
+++ src/documentations/repoDocumentation.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../css/repoDocumentation.css";
-import Image1 from '../images/repoImage1.png'
+import Image1 from '../images/repoImage1.png';
+import Image2 from '../images/repoImage2.png';
 
 function RepoDocumentation(){
     return(
@@ -19,8 +20,12 @@ function RepoDocumentation(){
                     Make sure that you installed <span style={{fontWeight: "bolder"}}>git</span>.
                     <p>Click <a className="repoLink" style={{color:"#16cdfa"}} href="https://git-scm.com/downloads">here </a>to install git.</p>
                 </li>
-                
+                <li>
+                    Mention the below steps.
+                </li>
             </ol>
+            <div style={{marginLeft: "20px"}}><img src={Image2} width={"80%"} height={"80%"}/></div>
+            <p style={{marginLeft:"25px", marginTop:"20px"}}>Add the command <span style={{color: "#16cdfa", fontWeight: "bold"}}>"git add ." </span>before commiting, so to add all the files to the repository.</p>
         </div>    
     )
 }
