28cd79f77f2214592260a538276f74577273e38c
Nivas <heroku.nivas.deployments@gmail.com>
2022-10-25 11:12:12 +0530

    Added all required values

--- src/components/Documentation.jsx
+++ src/components/Documentation.jsx
@@ -11,6 +11,15 @@ function Documentation(){
             <div className="arrowBackIconDoc">
                <Link to="/home"><ArrowBackIcon style={{color: "#16cdfa", marginTop: "10px", marginLeft: "18px"}}/></Link>
             </div>
+            <div id="apiDocumentation">
+                This is for api
+            </div>
+            <div id="repoDocumentation">
+                This is for repo
+            </div>
+            <div id="outputDocumentation">
+                This is for output
+            </div>
         </div>
 
     )
