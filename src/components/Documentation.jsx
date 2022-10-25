511bed731f4c69a065e75b018194d1928bfd154a
Nivas <heroku.nivas.deployments@gmail.com>
2022-10-25 11:33:03 +0530

    On the way

--- src/components/Documentation.jsx
+++ src/components/Documentation.jsx
@@ -3,6 +3,7 @@ import Navbar2 from "./Navbar2";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import '../css/Documentation.css';
 import { Link } from "react-router-dom";
+import ApiDocumentation from "../documentations/apiDocumentation";
 
 function Documentation(){
     return(
@@ -12,10 +13,10 @@ function Documentation(){
                <Link to="/home"><ArrowBackIcon style={{color: "#16cdfa", marginTop: "10px", marginLeft: "18px"}}/></Link>
             </div>
             <div className="gettingStarted">
-                <h5 style={{color: "#16cdfa"}}>To know what is Inverted Index of a file click <a style={{color: "#16cdfa"}} href="https://www.geeksforgeeks.org/inverted-index/"><span className="firstLink" style={{fontWeight: "bold"}}>here</span></a> </h5>
+                <h5 style={{color: "black"}}>To know what is Inverted Index of a file click <a style={{color: "black"}} href="https://www.geeksforgeeks.org/inverted-index/"><span className="firstLink" style={{fontWeight: "bold"}}>here</span></a> </h5>
             </div>
             <div id="apiDocumentation">
-                This is for api
+                <ApiDocumentation/>
             </div>
             <div id="repoDocumentation">
                 This is for repo
--- src/documentations/apiDocumentation.jsx
+++ src/documentations/apiDocumentation.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+import '../css/apiDocumentation.css';
+
+function ApiDocumentation(){
+    return(
+        <div className="apidoc">
+            <h3 style={{color: "#16cdfa", marginBottom: "1.8rem"}}>Welcome to API documentation</h3>
+        </div>
+    )
+}
+
+export default ApiDocumentation;
