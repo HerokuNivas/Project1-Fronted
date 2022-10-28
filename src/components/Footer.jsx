e51ff429cc20cc2452a8232bd679de7a2559d5f6
HerokuNivas <heroku.nivas.deployments@gmail.com>
2022-10-28 12:30:24 +0530

    Made capitalize

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     return(
         <div className="footer">
             <span className="footerText">Made with ❤️ in India</span>
-            <span style={{display: "block"}}>Copyright ©️ All rights reserved</span>
+            <span style={{display: "block"}}>Copyright ©️ All Rights Reserved</span>
         </div>    
     )
 }
