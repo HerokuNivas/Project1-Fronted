2a04625c2d2564a7bb6f110b746cfa8ab50aabaa
HerokuNivas <heroku.nivas.deployments@gmail.com>
2022-10-28 12:49:23 +0530

    Made changes

--- src/Errorandsuccess/success.tsx
+++ src/Errorandsuccess/success.tsx
@@ -7,6 +7,7 @@ import CloseIcon from '@mui/icons-material/Close';
 
 export default function SuccessAlert(props) {
     return (
+      /* This is the stack which contains the success message code */
       <Stack sx={{ width: '100%' }} spacing={2} style={{marginTop: "1.8rem"}}>
         <Alert severity="success">
           <AlertTitle>Success<CloseIcon style={{position: "absolute", right: "20px"}} onClick={() => {props.popup(false)}}/></AlertTitle>
